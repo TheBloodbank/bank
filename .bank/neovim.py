c71aeb11bae17dc1e91db0857b2d2112d9f8b621
Sam Hart <sam.hart@angieslist.com>
2016-02-24 11:03:16 -0500

    test

--- .bank/neovim.py
+++ .bank/neovim.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 class Package:
 
     def __init__(self):
         pass
+
+    def test(self):
+        print('Test!')
